d6d64608f130551ddc60f243f5e5b99b8a56c084
William Lew <WilliamMilesLew@gmail.com>
2020-02-12 23:12:31 -0800

    added: circular shift

--- src/fourier_transform.cu
+++ src/fourier_transform.cu
@@ -2,6 +2,28 @@
 #include <cmath>
 
 __global__
+void circularShift(float* dst, float* src, int width, int height, int shiftX, int shiftY) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int idy = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (idx >= width || idy >= height) { return; }
+
+  dst[idy * width + idx] = src[(((idy - shiftY) % height + height) % height) * width + (((idx - shiftX) % width + width) % width)];
+}
+
+// 0 1 2 3 4
+// 3 4 0 1 2
+
+// len = 5
+// shift = 2
+
+// [0] = [3] x - 2 mod 5
+// [1] = [4] x - 2 mod 5
+// [2] = [0] x - 2 mod 5
+// [3] = [1] x - 2 mod 5
+// [4] = [2] x - 2 mod 5
+
+__global__
 void fourierTransform(float* dst, float* src, int width, int height) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   int idy = blockIdx.y * blockDim.y + threadIdx.y;
@@ -24,42 +46,47 @@ extern "C" void fourierTransformWrapper(unsigned char* dst, unsigned char* src, 
   dim3 block(32, 32);
   dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
 
-  float *h_dst = (float*)malloc(width*height*sizeof(float));
-  float *h_src = (float*)malloc(width*height*sizeof(float));
+  float *h_circularFourierImage = (float*)malloc(width*height*sizeof(float));
+  float *h_image = (float*)malloc(width*height*sizeof(float));
 
   for (int x = 0; x < width * height * channels; x += channels) {
-    h_src[x / channels] = src[x] / 255.0;
+    h_image[x / channels] = src[x] / 255.0;
   }
 
-  float *d_dst, *d_src;
-  cudaMalloc(&d_dst, width*height*sizeof(float));
-  cudaMalloc(&d_src, width*height*sizeof(float));
-  cudaMemcpy(d_src, h_src, width*height*sizeof(float), cudaMemcpyHostToDevice);
+  float *d_fourierImage, *d_image;
+  cudaMalloc(&d_fourierImage, width*height*sizeof(float));
+  cudaMalloc(&d_image, width*height*sizeof(float));
+  cudaMemcpy(d_image, h_image, width*height*sizeof(float), cudaMemcpyHostToDevice);
 
-  fourierTransform<<<block, grid>>>(d_dst, d_src, width, height);
+  fourierTransform<<<block, grid>>>(d_fourierImage, d_image, width, height);
   cudaDeviceSynchronize();
 
-  cudaMemcpy(h_dst, d_dst, width*height*sizeof(float), cudaMemcpyDeviceToHost);
+  float *d_circularFourierImage;
+  cudaMalloc(&d_circularFourierImage, width*height*sizeof(float));
+  circularShift<<<block, grid>>>(d_circularFourierImage, d_fourierImage, width, height, width / 2, height / 2);
+
+  cudaMemcpy(h_circularFourierImage, d_circularFourierImage, width*height*sizeof(float), cudaMemcpyDeviceToHost);
 
   float max = 0.0;
   for (int x = 0; x < width * height; x++) {
-    if (h_dst[x] > max) {
-      max = h_dst[x];
+    if (h_circularFourierImage[x] > max) {
+      max = h_circularFourierImage[x];
     }
   }
 
   float c = 255.0 / log(1 + fabs(max));
   for (int x = 0; x < width * height; x++) {
-    h_dst[x] = c * log(1 + fabs(h_dst[x]));
+    h_circularFourierImage[x] = c * log(1 + fabs(h_circularFourierImage[x]));
 
-    dst[x * channels] = h_dst[x];
-    dst[x * channels + 1] = h_dst[x];
-    dst[x * channels + 2] = h_dst[x];
+    dst[x * channels] = h_circularFourierImage[x];
+    dst[x * channels + 1] = h_circularFourierImage[x];
+    dst[x * channels + 2] = h_circularFourierImage[x];
     dst[x * channels + 3] = 255;
   }
 
-  cudaFree(d_dst);
-  cudaFree(d_src);
-  free(h_dst);
-  free(h_src);
+  cudaFree(d_circularFourierImage);
+  cudaFree(d_fourierImage);
+  cudaFree(d_image);
+  free(h_circularFourierImage);
+  free(h_image);
 }
