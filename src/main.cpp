8b5e216093e1dd4a7482ed2f6df3c5106a1a073a
William Lew <WilliamMilesLew@gmail.com>
2020-02-12 18:15:12 -0800

    fixed: memory leak from not deallocating imageFourier

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include <stdio.h>
+
+#include "stb_image.h"
+#include "stb_image_write.h"
 
 extern "C" {
   void fourierTransformWrapper(unsigned char* dst, unsigned char* src, int width, int height, int channels);
@@ -10,10 +13,12 @@ extern "C" {
 int main(int argn, char** argv) {
   int width, height, comp;
   unsigned char* image = stbi_load(argv[1], &width, &height, &comp, STBI_rgb_alpha);
-  unsigned char* imageFourier = (unsigned char*)malloc(width * height * 4 * sizeof(unsigned char));
+  unsigned char* imageFourier = (unsigned char*)malloc(width*height*4*sizeof(unsigned char));
 
   fourierTransformWrapper(imageFourier, image, width, height, 4);
+  stbi_write_png("test.png", width, height, 4, imageFourier, width*4*sizeof(unsigned char));
 
+  free(imageFourier);
   stbi_image_free(image);
   return 0;
 }
