9ef6cf08ac4e6f124d69354b4a4833c204b1924c
William Lew <WilliamMilesLew@gmail.com>
2020-02-14 00:53:27 -0800

    added: image scaling

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,32 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
 
 #include <stdio.h>
 
 #include "stb_image.h"
 #include "stb_image_write.h"
+#include "stb_image_resize.h"
 
 extern "C" {
   void fourierTransformWrapper(unsigned char* dst, unsigned char* src, int width, int height, int channels);
 }
 
 int main(int argn, char** argv) {
+  int fourierWidth = 500, fourierHeight = 500, channels = 4;
+
   int width, height, comp;
   unsigned char* image = stbi_load(argv[1], &width, &height, &comp, STBI_rgb_alpha);
-  unsigned char* imageFourier = (unsigned char*)malloc(width*height*4*sizeof(unsigned char));
+  unsigned char* imageScaled = (unsigned char*)malloc(fourierWidth*fourierHeight*channels*sizeof(unsigned char));
 
-  fourierTransformWrapper(imageFourier, image, width, height, 4);
-  stbi_write_png(argv[2], width, height, 4, imageFourier, width*4*sizeof(unsigned char));
+  stbir_resize_uint8(image, width, height, 0, imageScaled, fourierWidth, fourierHeight, 0, channels);
 
-  free(imageFourier);
+  unsigned char* imageFourier = (unsigned char*)malloc(fourierWidth*fourierHeight*4*sizeof(unsigned char));
+  fourierTransformWrapper(imageFourier, imageScaled, fourierWidth, fourierHeight, channels);
+
+  stbi_write_png(argv[2], fourierWidth, fourierHeight, channels, imageFourier, fourierWidth*channels*sizeof(unsigned char));
+
+  free(imageScaled);
   stbi_image_free(image);
   return 0;
 }
