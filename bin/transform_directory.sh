06e825355ccb91c6adacee0629536e2c5da020dd
William Lew <WilliamMilesLew@gmail.com>
2020-02-16 18:32:27 -0800

    added: automated script to convert entire directory to fourier transformed image

--- bin/transform_directory.sh
+++ bin/transform_directory.sh
@@ -1,1 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ $# -le 1 ]
+then
+	echo "Usage: ./transform_directory INPUT_DIRECTORY... OUTPUT_DIRECTORY..."
+	echo "Fourier transform a directory of images"
+	echo ""
+else
+	mkdir -p $2
+	declare -i number=0
+
+	for file in $1/*;
+	do
+		$(dirname "$0")/fourier_transform.out $file $2/$number.png 500 500
+		number=$((number+1))
+	done
+fi
