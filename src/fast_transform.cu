abe71aff17c88d209f629f4b835421fc41e5a1cc
William Lew <WilliamMilesLew@gmail.com>
2020-03-03 12:26:11 -0800

    cleaned: arrays to pointers

--- src/fast_transform.cu
+++ src/fast_transform.cu
@@ -2,10 +2,10 @@
 #include <cmath>
 #include <complex>
 
-void recursiveFourierTransformCPU(std::complex<double> buf[], std::complex<double> out[], int n, int step) {
+void recursiveFastFourierTransformCPU(std::complex<double>* buf, std::complex<double>* out, int n, int step) {
   if (step < n) {
-    recursiveFourierTransformCPU(out, buf, n, step * 2);
-    recursiveFourierTransformCPU(out + step, buf + step, n, step * 2);
+    recursiveFastFourierTransformCPU(out, buf, n, step * 2);
+    recursiveFastFourierTransformCPU(out + step, buf + step, n, step * 2);
  
     for (int i = 0; i < n; i += 2 * step) {
       std::complex<double> I(0, 1);
@@ -21,9 +21,9 @@ void fastFourierTransformCPU(float* dst, float* src, int width, int height) {
   std::complex<double> buffer[] = {1, 1, 1, 1, 0, 0, 0, 0};
   std::complex<double> bufferClone[] = {1, 1, 1, 1, 0, 0, 0, 0};
 
-  recursiveFourierTransformCPU(buffer, bufferClone, 8, 1);
+  recursiveFastFourierTransformCPU(buffer, bufferClone, 8, 1);
 
   for (int x = 0; x < 8; x++) {
-    printf("%lf %lf\n", buffer[x].real(), buffer[x].imag());
+    printf("%f\n", sqrt((buffer[x].real() * buffer[x].real()) + (buffer[x].imag() * buffer[x].imag())));
   }
 }
